db653819b2bdf01d17cfe6c984bb9fcde63b1f9d
Christopher De Jesus <Christopher2653@gmail.com>
2022-03-15 15:20:33 -0400

    Hiding implementation details, added __all__

--- g_logger_utils/__init__.py
+++ g_logger_utils/__init__.py
@@ -1,40 +1,40 @@
-__all__ = []
-
-__version_info__ = (0, 8, 0)
+__version_info__ = (0, 8, 1)
 __version__ = '.'.join(map(str, __version_info__))
 
-import argparse
-import logging
-import os
-from typing import Optional
+__all__ = ['read_level_from_args', 'get_default_logger']
+
+import argparse as _argparse
+import logging as _logging
+import os as _os
+from typing import Optional as _Optional
 
 
 def read_level_from_args() -> int:
     """
     Called when a module wants to get log level using arguments.
     """
-    parser = argparse.ArgumentParser()
+    parser = _argparse.ArgumentParser()
     parser.add_argument('-l', '--log', default='INFO', type=str)
     args, _ = parser.parse_known_args()
     log_level = args.log
-    numeric_level = getattr(logging, log_level.upper(), None)
+    numeric_level = getattr(_logging, log_level.upper(), None)
     if not isinstance(numeric_level, int):
         raise ValueError(f'Invalid log level: {log_level}')
     return numeric_level
 
 
-if os.environ.get('READ_LOG_ARGS', False):
+if _os.environ.get('READ_LOG_ARGS', False):
     default_log_level = read_level_from_args()
 else:
-    default_log_level = logging.WARNING
-print(f'Default log level set to: {logging.getLevelName(default_log_level)}')
+    default_log_level = _logging.WARNING
+print(f'Default log level set to: {_logging.getLevelName(default_log_level)}')
 
 
 def get_default_logger(name: str, format_: str = '[%(asctime)s] [%(name)s]: '
                                                  '[%(levelname)s] %(message)s',
-                       filepath: Optional[str] = './output.log',
+                       filepath: _Optional[str] = './output.log',
                        date_format: str = '%m-%d-%Y %H:%M:%S',
-                       truncate_name: bool = True) -> logging.Logger:
+                       truncate_name: bool = True) -> _logging.Logger:
     """
     Using the provided arguments, gets an instance of a logger of the given
     name and sets it up.
@@ -59,19 +59,19 @@ def get_default_logger(name: str, format_: str = '[%(asctime)s] [%(name)s]: '
     -
         Returns the logger.
     """
-    formatter = logging.Formatter(format_, datefmt=date_format)
+    formatter = _logging.Formatter(format_, datefmt=date_format)
 
-    logger = logging.getLogger(name.split('.')[-1] if truncate_name else name)
+    logger = _logging.getLogger(name.split('.')[-1] if truncate_name else name)
     logger.setLevel(default_log_level)
 
-    ch = logging.StreamHandler()
+    ch = _logging.StreamHandler()
     ch.setLevel(default_log_level)
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
     if filepath is not None:
-        fh = logging.FileHandler(filepath)
-        fh.setLevel(logging.INFO)
+        fh = _logging.FileHandler(filepath)
+        fh.setLevel(_logging.INFO)
         fh.setFormatter(formatter)
         logger.addHandler(fh)
 
