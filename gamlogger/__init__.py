05cf2f6d40ad2a34880563ed40b60e1d88b3684c
Christopher De Jesus <Christopher2653@gmail.com>
2022-05-13 02:26:39 -0400

    Fixed a potential vulnerability involving environment variables and log levels. Also added utiltiy function `get_log_level`.

--- gamlogger/__init__.py
+++ gamlogger/__init__.py
@@ -9,6 +9,32 @@ import os as _os
 from typing import Optional as _Optional
 
 
+default_log_level = _logging.WARNING
+
+
+def get_log_level(level) -> int:
+    """
+    Returns the log level (int) for the given string or int.
+    PARAMETERS
+    ----------
+    level
+        The log level as either a string or an int.
+
+    RETURNS
+    -------
+    -
+        The log level as an int.
+    """
+    if isinstance(level, int):
+        return level
+    try:
+        level = level.upper()
+    except AttributeError:
+        raise ValueError(f'level={level}\nlevel must be of type int or str. '
+                         f'{type(level)} given instead.')
+    return _logging.getLevelName(level)
+
+
 def read_level_from_args() -> int:
     """
     Called when a module wants to get log level using arguments.
@@ -17,17 +43,10 @@ def read_level_from_args() -> int:
     parser.add_argument('-l', '--log', default='INFO', type=str)
     args, _ = parser.parse_known_args()
     log_level = args.log
-    numeric_level = getattr(_logging, log_level.upper(), None)
+    numeric_level = get_log_level(log_level)
     if not isinstance(numeric_level, int):
         raise ValueError(f'Invalid log level: {log_level}')
     return numeric_level
-
-
-if _os.environ.get('READ_LOG_ARGS', False):
-    default_log_level = read_level_from_args()
-else:
-    default_log_level = _logging.WARNING
-print(f'Default log level set to: {_logging.getLevelName(default_log_level)}')
 
 
 def get_default_logger(name: str, format_: str = '[%(asctime)s] [%(name)s]: '
@@ -59,20 +78,28 @@ def get_default_logger(name: str, format_: str = '[%(asctime)s] [%(name)s]: '
     -
         Returns the logger.
     """
+    logger.info('Creating a logger for %s.', name)
     formatter = _logging.Formatter(format_, datefmt=date_format)
 
-    logger = _logging.getLogger(name.split('.')[-1] if truncate_name else name)
-    logger.setLevel(default_log_level)
+    logger_ = _logging.getLogger(name.split('.')[-1]
+                                 if truncate_name else name)
+    logger_.setLevel(default_log_level)
 
     ch = _logging.StreamHandler()
     ch.setLevel(default_log_level)
     ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    logger_.addHandler(ch)
 
     if filepath is not None:
         fh = _logging.FileHandler(filepath)
         fh.setLevel(_logging.INFO)
         fh.setFormatter(formatter)
-        logger.addHandler(fh)
+        logger_.addHandler(fh)
 
-    return logger
+    logger.info('Logger created for %s.', name)
+
+    return logger_
+
+
+logger = get_default_logger(__name__, filepath=None)
+logger.setLevel(_logging.ERROR)
